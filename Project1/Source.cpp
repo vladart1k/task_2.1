9e307318ef13312f51dc179903023b379a406d8d
vladuhartik <vlad.art.007@gmail.com>
2020-10-13 12:55:34 +0300

    first commit

---
+++ Project1/Source.cpp
@@ -0,0 +1,24 @@
+// Lab_02.cpp // < прізвище, ім’я автора > // Лабораторна робота № 2. // Лінійні програми. // Варіант 0.1 
+
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+    double x;  // вхідний параметр    
+    double z1; // результат обчислення 1-го виразу   
+    //double z2; // результат обчислення 2-го виразу 
+    double Pi = 4 * atan(1.);
+
+    cout << "x = "; cin >> x;
+
+    z1 = cos(x) + sin(x) + cos(3*x) + sin(3*x);
+    //z2 = 2*sqrt(2)*cos(x)*sin(Pi/4 + 2*x);
+
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    //cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    return 0;
+}
